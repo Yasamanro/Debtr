1d2bc9f62ce3050dfa178579ed2deab963cf40b1
Yasamanro <yasaman.rohani@gmail.com>
2022-03-22 18:39:46 -0400

    Added index

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
-app = flask (__name__)
+app = Flask(__name__)
 
 @app.route("/")
-def home():
-	return "this is the main page"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
