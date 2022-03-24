4800ea2178811cf22a03b416b6e0a299b9e46e00
Filip Miscevic <FilipMiscevic@users.noreply.github.com>
2022-03-24 14:54:37 -0400

    Completed Splitwise mining.

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from home.views import home_view
 import itertools
 from splitwise import Splitwise	
 import requests
+import numpy as np
 
 app = Flask(__name__)  # Create application object
 
 # Splitwise credentials
 client_id = "iA4axoPLQVBOUOilnnS9XTNATl24sjOQITrNIt2h"
 s = Splitwise(client_id,"ffBc3UncyGtK8dS6coOOc7EiJz4qMW2zHjE6Cu0B")
+group_id = 12328749
 
 @app.route("/")
 def home():
@@ -17,7 +19,32 @@ def home():
 @app.route('/welcome')
 def render_welcome():
 	user = s.getCurrentUser()
-	return(f'Welcome {user.getFirstName()}')
+
+	#groups = s.getGroups()
+	#print([(group.getName(),group.getId()) for group in groups])
+
+	expenses = s.getExpenses(group_id=group_id,limit=255)
+	print(len(expenses))
+	group = s.getGroup(group_id)
+	global currency
+	currency = group.country_code if group.country_code else 'CAD'
+	global people
+	people = [user.getFirstName() for user in group.getMembers()]
+	people_dict = {user.getId():user.getFirstName() for user in group.getMembers()}
+	formatted_expenses = [[(people_dict[payment.getFromUser()],
+		people_dict[payment.getToUser()],
+		payment.getAmount()) for payment in expense.getRepayments() if payment.getFromUser() in people_dict and payment.getToUser() in people_dict] for expense in expenses]
+	
+	formatted_expenses = [e for e in formatted_expenses if len(e) != 0]
+	print(formatted_expenses)
+
+	debts = np.concatenate(formatted_expenses)
+
+	#getCurrencyCode
+
+	return(f'Welcome {user.getFirstName()}<br>\
+			Your group users are: {people}<br>\
+			Your current expenses are: {show_transactions(simplify_debts(debts))}')
 
 @app.route("/authorize")
 def authorize():
@@ -53,8 +80,9 @@ def create_app(config_file):
 	app.register_blueprint(home_view)  # Register url's so application knows what to do
 	return app
 
+# TODO: mine data from Splitwise
 # Code for simplifying debt taken from https://terbium.io/2020/09/debt-simplification/
-
+'''
 people = ["Grace", "Ivan", "Judy", "Luke", "Mallory"]
 debts = [
     ("Grace", "Ivan", 5),
@@ -70,19 +98,22 @@ debts = [
     ("Mallory", "Luke", 6),
     ("Mallory", "Judy", 11),
 ]
+'''
 
 def show_transactions(transactions):
-    for (debtor, creditor, value) in transactions:
-        if value > 0:
-            print(f"{debtor} owes {creditor} ${value}")
-        else:
-            print(f"{creditor} owes {debtor} ${-value}")
+	transaction_list = []
+	for (debtor, creditor, value) in transactions:
+		if value > 0:
+			transaction_list.append(f"{debtor} owes {creditor} ${value}")
+		else:
+			transaction_list.append(f"{creditor} owes {debtor} ${-value}")
+	return transaction_list
 
 def compute_balances(debts):
     balances = {person: 0 for person in people}
     for (debtor, creditor, value) in debts:
-        balances[debtor] -= value
-        balances[creditor] += value
+        balances[debtor] -= float(value)
+        balances[creditor] += float(value)
     return balances
 
 def simplify_with_collector(balances):
@@ -115,6 +146,5 @@ def simplify_debts(debts):
 
 if __name__ == "__main__":
 
-	show_transactions(simplify_debts(debts))	
 	a = create_app('settingslocal.py')  # Create application with our config file
 	a.run()  # Run our application
